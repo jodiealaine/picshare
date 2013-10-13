9fce7d6720f6081d093ad2bb8057b3008035c9e4
Mindful Coder <jodie@mindfulcoder.com>
2013-10-13 20:07:25 +0100

    Added Home, About, Bootstrap-sass styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picshare::Application.config.secret_key_base = 'd011a7ecdecd6da3ae8229ca5fd925a63b8a4876ddb24a72fd566d6cea15b83fc483ba68c9c67179e018664e15722d4600a477dd62b84e523fe18144b721a624'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Picshare::Application.config.session_store :cookie_store, key: '_picshare_session'
