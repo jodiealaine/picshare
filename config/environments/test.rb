af29137d4c4d266b62bbdabadc659607776dcf22
Mindful Coder <jodie@mindfulcoder.com>
2013-10-13 20:56:26 +0100

    Add Devise Views, Devise User Model

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Picshare::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  
+  config.action_mailer.default_url_options={:host => 'http://localhost:3000'}
 end
